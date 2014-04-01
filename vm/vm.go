6cdbe7fb6495f07cbade8128f19c37affbcc57f6
mattn <mattn.jp@gmail.com>
2014-04-01 11:39:31 +0900

    a = b, c, a should be b not slice of b, c

--- vm/vm.go
+++ vm/vm.go
@@ -360,6 +360,11 @@ func invokeExpr(expr ast.Expr, env *Env) (reflect.Value, error) {
 		if err != nil {
 			return NilValue, newError(err, expr)
 		}
+		if rv.Kind() == reflect.Array || rv.Kind() == reflect.Slice {
+			if rv.Len() > 0 {
+				rv = rv.Index(0)
+			}
+		}
 		if env.Set(e.Name, rv) != nil {
 			env.Define(e.Name, rv)
 		}
