33e97ff0c2062491335d17ce2ac5b09371ea93c4
mattn <mattn.jp@gmail.com>
2014-04-02 09:52:21 +0900

    Exit if build fail

--- test/test.bat
+++ test/test.bat
@@ -3,6 +3,7 @@
 setlocal enabledelayedexpansion
 set DIR=%~dp0
 (cd %DIR%.. && go build)
+if !ERRORLEVEL! neq 0 goto error
 for %%i in (%DIR%\*.t) do (
   %DIR%..\anko %DIR%\tester.ank %%i
   if !ERRORLEVEL! neq 0 goto error
