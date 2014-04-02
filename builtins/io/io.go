986e2e8cd3629885882ca2cb82a132d33c858ee2
mattn <mattn.jp@gmail.com>
2014-04-02 13:06:54 +0900

    Provide only io

--- builtins/io/io.go
+++ builtins/io/io.go
@@ -3,29 +3,13 @@ package io
 
 import (
 	"errors"
+	"github.com/mattn/anko/builtins/io/ioutil"
 	"github.com/mattn/anko/vm"
-	i "io"
-	u "io/ioutil"
 	"reflect"
 )
 
 func Import(env *vm.Env) {
 	m := env.NewModule("io")
-	m.Define("ReadAll", vm.ToFunc(func(args ...reflect.Value) (reflect.Value, error) {
-		if len(args) < 1 {
-			return vm.NilValue, errors.New("Missing arguments")
-		}
-		if len(args) > 1 {
-			return vm.NilValue, errors.New("Too many arguments")
-		}
-		r, ok := args[0].Interface().(i.Reader)
-		if !ok {
-			return vm.NilValue, errors.New("Argument should be Reader")
-		}
-		b, err := u.ReadAll(r)
-		if !ok {
-			return vm.NilValue, err
-		}
-		return reflect.ValueOf(b), nil
-	}))
+
+	ioutil.Import(m)
 }
