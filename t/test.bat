b6f43ec1ce03e11b5d52ea50301748e62cdefb2f
mattn <mattn.jp@gmail.com>
2014-04-04 16:41:55 +0900

    Duplicate backslash

--- t/test.bat
+++ t/test.bat
@@ -4,8 +4,8 @@ setlocal enabledelayedexpansion
 set DIR=%~dp0
 (cd %DIR%.. && go build)
 if !ERRORLEVEL! neq 0 goto error
-for %%i in (%DIR%\*.t) do (
-  %DIR%..\anko %DIR%\tester.ank %%i
+for %%i in (%DIR%*.t) do (
+  %DIR%..\anko %DIR%tester.ank %%i
   if !ERRORLEVEL! neq 0 goto error
 )
 exit /b 0
