73a9c81f285321ca0fb4e82b9f52bdf0db24387b
MichaelS11 <MichaelS11@users.noreply.github.com>
2018-01-23 19:07:48 -0800

    Added tests to TestJson

--- builtins/core_test.go
+++ builtins/core_test.go
@@ -131,6 +131,8 @@ func TestJson(t *testing.T) {
 	tests := []testStruct{
 		{script: "json = import(\"encoding/json\"); a = {\"b\": \"b\"}; c, err = json.Marshal(a); if err == nil { return true }", runOutput: true, output: map[string]interface{}{"a": map[string]interface{}{"b": "b"}, "c": []byte("{\"b\":\"b\"}")}},
 		{script: "json = import(\"encoding/json\"); b = 1; err = json.Unmarshal(a, &b); if err == nil { return true }", input: map[string]interface{}{"a": []byte("{\"b\": \"b\"}")}, runOutput: true, output: map[string]interface{}{"a": []byte("{\"b\": \"b\"}"), "b": map[string]interface{}{"b": "b"}}},
+		{script: "json = import(\"encoding/json\"); b = 1; err = json.Unmarshal(toByteSlice(a), &b); if err == nil { return true }", input: map[string]interface{}{"a": "{\"b\": \"b\"}"}, runOutput: true, output: map[string]interface{}{"a": "{\"b\": \"b\"}", "b": map[string]interface{}{"b": "b"}}},
+		{script: "json = import(\"encoding/json\"); b = 1; err = json.Unmarshal(toByteSlice(a), &b); if err == nil { return true }", input: map[string]interface{}{"a": "[[\"1\", \"2\"],[\"3\", \"4\"]]"}, runOutput: true, output: map[string]interface{}{"a": "[[\"1\", \"2\"],[\"3\", \"4\"]]", "b": []interface{}{[]interface{}{"1", "2"}, []interface{}{"3", "4"}}}},
 	}
 	runTests(t, tests)
 }
