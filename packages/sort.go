44d38af059757ba41223ec470d1cdff043af1b12
MichaelS11 <MichaelS11@users.noreply.github.com>
2018-03-06 19:38:44 -0800

    VM now supports custom sort functions Added sort SortFuncsStruct

--- packages/sort.go
+++ packages/sort.go
@@ -4,6 +4,16 @@ import (
 	"sort"
 )
 
+type SortFuncsStruct struct {
+	LenFunc  func() int
+	LessFunc func(i, j int) bool
+	SwapFunc func(i, j int)
+}
+
+func (s SortFuncsStruct) Len() int           { return s.LenFunc() }
+func (s SortFuncsStruct) Less(i, j int) bool { return s.LessFunc(i, j) }
+func (s SortFuncsStruct) Swap(i, j int)      { s.SwapFunc(i, j) }
+
 func init() {
 	Packages["sort"] = map[string]interface{}{
 		"Float64s":          sort.Float64s,
@@ -21,9 +31,10 @@ func init() {
 		"StringsAreSorted":  sort.StringsAreSorted,
 	}
 	PackageTypes["sort"] = map[string]interface{}{
-		"Float64Slice": sort.Float64Slice{},
-		"IntSlice":     sort.IntSlice{},
-		"StringSlice":  sort.StringSlice{},
+		"Float64Slice":    sort.Float64Slice{},
+		"IntSlice":        sort.IntSlice{},
+		"StringSlice":     sort.StringSlice{},
+		"SortFuncsStruct": &SortFuncsStruct{},
 	}
 	sortGo18()
 }
--- packages/sort_test.go
+++ packages/sort_test.go
@@ -11,6 +11,17 @@ func TestSort(t *testing.T) {
 		{script: "sort = import(\"sort\"); a = make([]\"int\"); a += [5, 3, 1, 4, 2]; sort.Ints(a); a", runOutput: []int{1, 2, 3, 4, 5}, output: map[string]interface{}{"a": []int{1, 2, 3, 4, 5}}},
 		{script: "sort = import(\"sort\"); a = make([]\"float64\"); a += [5.5, 3.3, 1.1, 4.4, 2.2]; sort.Float64s(a); a", runOutput: []float64{1.1, 2.2, 3.3, 4.4, 5.5}, output: map[string]interface{}{"a": []float64{1.1, 2.2, 3.3, 4.4, 5.5}}},
 		{script: "sort = import(\"sort\"); a = make([]\"string\"); a += [\"e\", \"c\", \"a\", \"d\", \"b\"]; sort.Strings(a); a", runOutput: []string{"a", "b", "c", "d", "e"}, output: map[string]interface{}{"a": []string{"a", "b", "c", "d", "e"}}},
+		{script: `
+sort = import("sort")
+a = [5, 1.1, 3, "f", "2", "4.4"]
+sortFuncs = make("sort.SortFuncsStruct")
+sortFuncs.LenFunc = func() { return len(a) }
+sortFuncs.LessFunc = func(i, j) { return a[i] < a[j] }
+sortFuncs.SwapFunc = func(i, j) { temp = a[i]; a[i] = a[j]; a[j] = temp }
+sort.Sort(sortFuncs)
+a
+`,
+			runOutput: []interface{}{"f", float64(1.1), "2", int64(3), "4.4", int64(5)}, output: map[string]interface{}{"a": []interface{}{"f", float64(1.1), "2", int64(3), "4.4", int64(5)}}},
 	}
 	runTests(t, tests)
 }
