1ad22a1307db24d2d539e9cd75d078478aab9635
MichaelS11 <MichaelS11@users.noreply.github.com>
2018-04-06 11:00:41 -0700

    Switch to Testing

--- vm/vmIfAndSwitch_test.go
+++ vm/vmIfAndSwitch_test.go
@@ -8,75 +8,77 @@ import (
 
 func TestIf(t *testing.T) {
 	os.Setenv("ANKO_DEBUG", "1")
-	tests := []testStruct{
-		{script: `if 1++ {}`, runError: fmt.Errorf("invalid operation")},
-		{script: `if false {} else if 1++ {}`, runError: fmt.Errorf("invalid operation")},
-		{script: `if false {} else if true { 1++ }`, runError: fmt.Errorf("invalid operation")},
+	tests := []Test{
+		{Script: `if 1++ {}`, RunError: fmt.Errorf("invalid operation")},
+		{Script: `if false {} else if 1++ {}`, RunError: fmt.Errorf("invalid operation")},
+		{Script: `if false {} else if true { 1++ }`, RunError: fmt.Errorf("invalid operation")},
 
-		{script: `if true {}`, input: map[string]interface{}{"a": nil}, runOutput: nil, output: map[string]interface{}{"a": nil}},
-		{script: `if true {}`, input: map[string]interface{}{"a": true}, runOutput: nil, output: map[string]interface{}{"a": true}},
-		{script: `if true {}`, input: map[string]interface{}{"a": int64(1)}, runOutput: nil, output: map[string]interface{}{"a": int64(1)}},
-		{script: `if true {}`, input: map[string]interface{}{"a": float64(1.1)}, runOutput: nil, output: map[string]interface{}{"a": float64(1.1)}},
-		{script: `if true {}`, input: map[string]interface{}{"a": "a"}, runOutput: nil, output: map[string]interface{}{"a": "a"}},
+		{Script: `if true {}`, Input: map[string]interface{}{"a": nil}, RunOutput: nil, Output: map[string]interface{}{"a": nil}},
+		{Script: `if true {}`, Input: map[string]interface{}{"a": true}, RunOutput: nil, Output: map[string]interface{}{"a": true}},
+		{Script: `if true {}`, Input: map[string]interface{}{"a": int64(1)}, RunOutput: nil, Output: map[string]interface{}{"a": int64(1)}},
+		{Script: `if true {}`, Input: map[string]interface{}{"a": float64(1.1)}, RunOutput: nil, Output: map[string]interface{}{"a": float64(1.1)}},
+		{Script: `if true {}`, Input: map[string]interface{}{"a": "a"}, RunOutput: nil, Output: map[string]interface{}{"a": "a"}},
 
-		{script: `if true {a = nil}`, input: map[string]interface{}{"a": int64(2)}, runOutput: nil, output: map[string]interface{}{"a": nil}},
-		{script: `if true {a = true}`, input: map[string]interface{}{"a": int64(2)}, runOutput: true, output: map[string]interface{}{"a": true}},
-		{script: `if true {a = 1}`, input: map[string]interface{}{"a": int64(2)}, runOutput: int64(1), output: map[string]interface{}{"a": int64(1)}},
-		{script: `if true {a = 1.1}`, input: map[string]interface{}{"a": int64(2)}, runOutput: float64(1.1), output: map[string]interface{}{"a": float64(1.1)}},
-		{script: `if true {a = "a"}`, input: map[string]interface{}{"a": int64(2)}, runOutput: "a", output: map[string]interface{}{"a": "a"}},
+		{Script: `if true {a = nil}`, Input: map[string]interface{}{"a": int64(2)}, RunOutput: nil, Output: map[string]interface{}{"a": nil}},
+		{Script: `if true {a = true}`, Input: map[string]interface{}{"a": int64(2)}, RunOutput: true, Output: map[string]interface{}{"a": true}},
+		{Script: `if true {a = 1}`, Input: map[string]interface{}{"a": int64(2)}, RunOutput: int64(1), Output: map[string]interface{}{"a": int64(1)}},
+		{Script: `if true {a = 1.1}`, Input: map[string]interface{}{"a": int64(2)}, RunOutput: float64(1.1), Output: map[string]interface{}{"a": float64(1.1)}},
+		{Script: `if true {a = "a"}`, Input: map[string]interface{}{"a": int64(2)}, RunOutput: "a", Output: map[string]interface{}{"a": "a"}},
 
-		{script: `if a == 1 {a = 1}`, input: map[string]interface{}{"a": int64(2)}, runOutput: false, output: map[string]interface{}{"a": int64(2)}},
-		{script: `if a == 2 {a = 1}`, input: map[string]interface{}{"a": int64(2)}, runOutput: int64(1), output: map[string]interface{}{"a": int64(1)}},
-		{script: `if a == 1 {a = nil}`, input: map[string]interface{}{"a": int64(2)}, runOutput: false, output: map[string]interface{}{"a": int64(2)}},
-		{script: `if a == 2 {a = nil}`, input: map[string]interface{}{"a": int64(2)}, runOutput: nil, output: map[string]interface{}{"a": nil}},
+		{Script: `if a == 1 {a = 1}`, Input: map[string]interface{}{"a": int64(2)}, RunOutput: false, Output: map[string]interface{}{"a": int64(2)}},
+		{Script: `if a == 2 {a = 1}`, Input: map[string]interface{}{"a": int64(2)}, RunOutput: int64(1), Output: map[string]interface{}{"a": int64(1)}},
+		{Script: `if a == 1 {a = nil}`, Input: map[string]interface{}{"a": int64(2)}, RunOutput: false, Output: map[string]interface{}{"a": int64(2)}},
+		{Script: `if a == 2 {a = nil}`, Input: map[string]interface{}{"a": int64(2)}, RunOutput: nil, Output: map[string]interface{}{"a": nil}},
 
-		{script: `if a == 1 {a = 1} else {a = 3}`, input: map[string]interface{}{"a": int64(2)}, runOutput: int64(3), output: map[string]interface{}{"a": int64(3)}},
-		{script: `if a == 2 {a = 1} else {a = 3}`, input: map[string]interface{}{"a": int64(2)}, runOutput: int64(1), output: map[string]interface{}{"a": int64(1)}},
-		{script: `if a == 1 {a = 1} else if a == 3 {a = 3}`, input: map[string]interface{}{"a": int64(2)}, runOutput: false, output: map[string]interface{}{"a": int64(2)}},
-		{script: `if a == 1 {a = 1} else if a == 2 {a = 3}`, input: map[string]interface{}{"a": int64(2)}, runOutput: int64(3), output: map[string]interface{}{"a": int64(3)}},
-		{script: `if a == 1 {a = 1} else if a == 3 {a = 3} else {a = 4}`, input: map[string]interface{}{"a": int64(2)}, runOutput: int64(4), output: map[string]interface{}{"a": int64(4)}},
+		{Script: `if a == 1 {a = 1} else {a = 3}`, Input: map[string]interface{}{"a": int64(2)}, RunOutput: int64(3), Output: map[string]interface{}{"a": int64(3)}},
+		{Script: `if a == 2 {a = 1} else {a = 3}`, Input: map[string]interface{}{"a": int64(2)}, RunOutput: int64(1), Output: map[string]interface{}{"a": int64(1)}},
+		{Script: `if a == 1 {a = 1} else if a == 3 {a = 3}`, Input: map[string]interface{}{"a": int64(2)}, RunOutput: false, Output: map[string]interface{}{"a": int64(2)}},
+		{Script: `if a == 1 {a = 1} else if a == 2 {a = 3}`, Input: map[string]interface{}{"a": int64(2)}, RunOutput: int64(3), Output: map[string]interface{}{"a": int64(3)}},
+		{Script: `if a == 1 {a = 1} else if a == 3 {a = 3} else {a = 4}`, Input: map[string]interface{}{"a": int64(2)}, RunOutput: int64(4), Output: map[string]interface{}{"a": int64(4)}},
 
-		{script: `if a == 1 {a = 1} else {a = nil}`, input: map[string]interface{}{"a": int64(2)}, runOutput: nil, output: map[string]interface{}{"a": nil}},
-		{script: `if a == 2 {a = nil} else {a = 3}`, input: map[string]interface{}{"a": int64(2)}, runOutput: nil, output: map[string]interface{}{"a": nil}},
-		{script: `if a == 1 {a = nil} else if a == 3 {a = nil}`, input: map[string]interface{}{"a": int64(2)}, runOutput: false, output: map[string]interface{}{"a": int64(2)}},
-		{script: `if a == 1 {a = 1} else if a == 2 {a = nil}`, input: map[string]interface{}{"a": int64(2)}, runOutput: nil, output: map[string]interface{}{"a": nil}},
-		{script: `if a == 1 {a = 1} else if a == 3 {a = 3} else {a = nil}`, input: map[string]interface{}{"a": int64(2)}, runOutput: nil, output: map[string]interface{}{"a": nil}},
+		{Script: `if a == 1 {a = 1} else {a = nil}`, Input: map[string]interface{}{"a": int64(2)}, RunOutput: nil, Output: map[string]interface{}{"a": nil}},
+		{Script: `if a == 2 {a = nil} else {a = 3}`, Input: map[string]interface{}{"a": int64(2)}, RunOutput: nil, Output: map[string]interface{}{"a": nil}},
+		{Script: `if a == 1 {a = nil} else if a == 3 {a = nil}`, Input: map[string]interface{}{"a": int64(2)}, RunOutput: false, Output: map[string]interface{}{"a": int64(2)}},
+		{Script: `if a == 1 {a = 1} else if a == 2 {a = nil}`, Input: map[string]interface{}{"a": int64(2)}, RunOutput: nil, Output: map[string]interface{}{"a": nil}},
+		{Script: `if a == 1 {a = 1} else if a == 3 {a = 3} else {a = nil}`, Input: map[string]interface{}{"a": int64(2)}, RunOutput: nil, Output: map[string]interface{}{"a": nil}},
 
-		{script: `if a == 1 {a = 1} else if a == 3 {a = 3} else if a == 4 {a = 4} else {a = 5}`, input: map[string]interface{}{"a": int64(2)}, runOutput: int64(5), output: map[string]interface{}{"a": int64(5)}},
-		{script: `if a == 1 {a = 1} else if a == 3 {a = 3} else if a == 4 {a = 4} else {a = nil}`, input: map[string]interface{}{"a": int64(2)}, runOutput: nil, output: map[string]interface{}{"a": nil}},
-		{script: `if a == 1 {a = 1} else if a == 3 {a = 3} else if a == 2 {a = 4} else {a = 5}`, input: map[string]interface{}{"a": int64(2)}, runOutput: int64(4), output: map[string]interface{}{"a": int64(4)}},
-		{script: `if a == 1 {a = 1} else if a == 3 {a = 3} else if a == 2 {a = nil} else {a = 5}`, input: map[string]interface{}{"a": int64(2)}, runOutput: nil, output: map[string]interface{}{"a": nil}},
+		{Script: `if a == 1 {a = 1} else if a == 3 {a = 3} else if a == 4 {a = 4} else {a = 5}`, Input: map[string]interface{}{"a": int64(2)}, RunOutput: int64(5), Output: map[string]interface{}{"a": int64(5)}},
+		{Script: `if a == 1 {a = 1} else if a == 3 {a = 3} else if a == 4 {a = 4} else {a = nil}`, Input: map[string]interface{}{"a": int64(2)}, RunOutput: nil, Output: map[string]interface{}{"a": nil}},
+		{Script: `if a == 1 {a = 1} else if a == 3 {a = 3} else if a == 2 {a = 4} else {a = 5}`, Input: map[string]interface{}{"a": int64(2)}, RunOutput: int64(4), Output: map[string]interface{}{"a": int64(4)}},
+		{Script: `if a == 1 {a = 1} else if a == 3 {a = 3} else if a == 2 {a = nil} else {a = 5}`, Input: map[string]interface{}{"a": int64(2)}, RunOutput: nil, Output: map[string]interface{}{"a": nil}},
 	}
-	runTests(t, tests)
+	RunTests(t, tests)
 }
 
 func TestSwitch(t *testing.T) {
 	os.Setenv("ANKO_DEBUG", "1")
-	tests := []testStruct{
-		{script: `switch {}`, parseError: fmt.Errorf("syntax error")},
-		{script: `a=1; switch a {case: return a}`, parseError: fmt.Errorf("syntax error")},
-		{script: `a=1; switch a; {}`, parseError: fmt.Errorf("syntax error")},
+	tests := []Test{
+		{Script: `switch {}`, ParseError: fmt.Errorf("syntax error")},
+		{Script: `a=1; switch a {case: return a}`, ParseError: fmt.Errorf("syntax error")},
+		{Script: `a=1; switch a; {}`, ParseError: fmt.Errorf("syntax error")},
 
-		{script: `a=1; switch a {}`, runOutput: int64(1)},
+		{Script: `a=1; switch a {}`, RunOutput: int64(1)},
 
-		{script: `a=1; switch a {case 1: return a}`, runOutput: int64(1)},
-		{script: `a=2; switch a {case 1,2: return a}`, runOutput: int64(2)},
-		{script: `a=3; switch a {case 1,2,3: return a}`, runOutput: int64(3)},
+		{Script: `a=1; switch a {case 1: return a}`, RunOutput: int64(1)},
+		{Script: `a=2; switch a {case 1,2: return a}`, RunOutput: int64(2)},
+		{Script: `a=3; switch a {case 1,2,3: return a}`, RunOutput: int64(3)},
 
-		{script: `a=2; switch a {case 1: return a;case 2: return a}`, runOutput: int64(2)},
-		{script: `a=3; switch a {case 1,3: return a;case 2: return a}`, runOutput: int64(3)},
-		{script: `a=3; switch a {case 1: return a;case 2,3: return a}`, runOutput: int64(3)},
+		{Script: `a=2; switch a {case 1: return a; case 2: return a}`, RunOutput: int64(2)},
+		{Script: `a=3; switch a {case 1,3: return a; case 2: return a}`, RunOutput: int64(3)},
+		{Script: `a=3; switch a {case 1: return a; case 2,3: return a}`, RunOutput: int64(3)},
 
-		{script: `a=99; switch a {default: return 'default'}`, runOutput: "default"},
-		{script: `a=99; switch a {case 1: return a;default: return 'default'}`, runOutput: "default"},
-		{script: `a=99; switch a {case 1,2: return a;default: return 'default'}`, runOutput: "default"},
-		{script: `a=99; switch a {case 1,2,3: return a;default: return 'default'}`, runOutput: "default"},
-		{script: `a=99; switch a {case 1: return a;case 2: return a;default: return 'default'}`, runOutput: "default"},
-		{script: `a=99; switch a {case 1,3: return a;case 2: return a;default: return 'default'}`, runOutput: "default"},
-		{script: `a=99; switch a {case 1: return a;case 2,3: return a;default: return 'default'}`, runOutput: "default"},
-		{script: `a=99; switch a {case 1: return a;default: return 'default';default: return 'default2'}`, parseError: fmt.Errorf("multiple default statement")},
-		{script: `a=99; switch a {case 1,2: return a;default: return 'default';default: return 'default2'}`, parseError: fmt.Errorf("multiple default statement")},
-		{script: `a=99; switch a {case 1: return a;case 2: return a;default: return 'default';default: return 'default2'}`, parseError: fmt.Errorf("multiple default statement")},
+		{Script: `a=99; switch a {default: return 'default'}`, RunOutput: "default"},
+		{Script: `a=99; switch a {case 1: return a; default: return 'default'}`, RunOutput: "default"},
+		{Script: `a=99; switch a {case 1,2: return a; default: return 'default'}`, RunOutput: "default"},
+		{Script: `a=99; switch a {case 1,2,3: return a; default: return 'default'}`, RunOutput: "default"},
+		{Script: `a=99; switch a {case 1: return a; case 2: return a; default: return 'default'}`, RunOutput: "default"},
+		{Script: `a=99; switch a {case 1,3: return a; case 2: return a; default: return 'default'}`, RunOutput: "default"},
+		{Script: `a=99; switch a {case 1: return a; case 2,3: return a; default: return 'default'}`, RunOutput: "default"},
+
+		// TODO: After a parse error, the second default statment still runs and returns. Is this something to fix?
+		{Script: `a=99; switch a {case 1: return a; default: return 'default'; default: return 'default2'}`, ParseError: fmt.Errorf("multiple default statement"), RunOutput: "default2"},
+		{Script: `a=99; switch a {case 1,2: return a; default: return 'default'; default: return 'default2'}`, ParseError: fmt.Errorf("multiple default statement"), RunOutput: "default2"},
+		{Script: `a=99; switch a {case 1: return a; case 2: return a; default: return 'default'; default: return 'default2'}`, ParseError: fmt.Errorf("multiple default statement"), RunOutput: "default2"},
 	}
-	runTests(t, tests)
+	RunTests(t, tests)
 }
