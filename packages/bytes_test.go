9cc7c122ec424f25fd7054e572ab59c05232da28
MichaelS11 <MichaelS11@users.noreply.github.com>
2018-04-07 08:59:25 -0700

    Added comment

--- packages/bytes_test.go
+++ packages/bytes_test.go
@@ -13,6 +13,7 @@ func TestBytes(t *testing.T) {
 		// TOFIX: no member named 'WriteString' for struct
 		// {Script: `bytes = import("bytes"); a = make(bytes.Buffer); n, err = a.WriteString("b")`, EnvSetupFunc: &testPackagesEnvSetupFunc, RunOutput: []interface{}{1, nil}},
 		// {Script: `bytes = import("bytes"); a = make(bytes.Buffer); n, err = a.WriteString("b"); a.String()`, EnvSetupFunc: &testPackagesEnvSetupFunc, RunOutput: "b"},
+		// Comment to trigger build
 	}
 	vm.RunTests(t, tests)
 }
