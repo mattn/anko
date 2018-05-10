14f6aa645869924034b79a5b4bffb3872c91aa4e
Michel Casabianca <casa@sweetohm.net>
2018-05-10 23:07:06 +0200

    Added Copy() function on vm.Env

---
+++ vm/copy.go
@@ -0,0 +1,20 @@
+package vm
+
+import "reflect"
+
+func (env *Env) Copy() *Env {
+	b := false
+	copy := Env {
+		env:       make(map[string]reflect.Value),
+		typ:       make(map[string]reflect.Type),
+		parent:    nil,
+		interrupt: &b,
+	}
+	for name, value := range env.env {
+		copy.env[name] = value
+	}
+	for name, typ := range env.typ {
+		copy.typ[name] = typ
+	}
+	return &copy
+}
