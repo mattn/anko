f34b48cf6b7c6e3bbfb6765015da59447a73f743
MichaelS11 <MichaelS11@users.noreply.github.com>
2018-05-12 11:58:42 -0700

    Added arrays example

--- vm/example_containers_test.go
+++ vm/example_containers_test.go
@@ -7,6 +7,70 @@ import (
 	"github.com/mattn/anko/vm"
 )
 
+func Example_vmArrays() {
+	env := vm.NewEnv()
+
+	err := env.Define("println", fmt.Println)
+	if err != nil {
+		log.Fatalf("Define error: %v\n", err)
+	}
+
+	script := `
+a = [1, 2]
+println(a)
+
+a += 3
+println(a)
+
+a = []
+// this automaticly appends to array
+a[0] = 1
+println(a)
+
+println("")
+
+a = []
+// this would give an index out of range error
+// a[1] = 1
+
+a = [1, 2]
+b = [3, 4]
+c = a + b
+println(c)
+
+c = [1, 2] + [3, 4]
+println(c)
+
+println("")
+
+c = [a] + b
+println(c)
+
+c = [a] + [b]
+println(c)
+
+c = [[1, 2]] + [[3, 4]]
+println(c)
+`
+
+	_, err = env.Execute(script)
+	if err != nil {
+		log.Fatalf("execute error: %v\n", err)
+	}
+
+	// output:
+	// [1 2]
+	// [1 2 3]
+	// [1]
+	//
+	// [1 2 3 4]
+	// [1 2 3 4]
+	//
+	// [[1 2] 3 4]
+	// [[1 2] [3 4]]
+	// [[1 2] [3 4]]
+}
+
 func Example_vmModule() {
 	env := vm.NewEnv()
 
